74f0631bcc52b9009ee117f86ff2fb438cdff10d
ChuHieuMagic <23120258@student.hcmus.edu.vn>
2024-11-14 03:43:13 +0700

    Finished most of the frame

--- main.cpp
+++ main.cpp
@@ -1,37 +1,185 @@
-#include <vector>
-#include "data_generator.cpp"
+#include "main.h"
 
-using namespace std; 
+using namespace std;
 
 int main()
 {
     //Declare Data Order and Data Size
-    vector<int> n = {10000, 30000, 50000, 100000, 300000, 500000};
+    vector<int> data_sizes = {10, 30, 50, 100, 300, 500};
     vector<int*> sorted, nearly_sorted, reverse_sorted, randomized;
+    vector<vector<int*>> data_orders = {sorted, nearly_sorted, reverse_sorted, randomized};
+    vector<string> data_order_names = {"Randomized", "Sorted", "Reverse Sorted", "Nearly Sorted"};
+    vector<string> algorithms = {"SelectionSort", "InsertionSort"}; //Finished Algorithm
+    //"SelectionSort", "InsertionSort", "HeapSort", "MergeSort", "QuickSort",
+    //"RadixSort", "Counting", "BinaryInsertionSort", "ShakerSort", "FlashSort"
+    //Add after finish any algorithm
+    
+    //Write the result (running time and number of comparisons) in a result file
+    ofstream fout("sorting_result.txt");
+    if (!fout.is_open()) cout << "Cant open result file.\n";
 
-    //Generate data for each Data Order. 
-    //Size of each array based on index [0]: 10000, [1]: 30000, [2]50000, [3]100000, [4]300000, [5]500000.
-    for(int i = 0; i < n.size(); i++){
-        //Initialize each data order with size n[i]
-        int* sorted_array = new int[n[i]];
-        int* nearly_sorted_array = new int[n[i]];
-        int* reverse_sorted_array = new int[n[i]];
-        int* randomized_array = new int[n[i]];
+    //Controler Variables
+    int data_type = 0;
+    int sort_type = 0;
+    int nameIndex = 0;
+    // for each Data Order S1
+    for (vector<int*> data_order : data_orders){
+        string data_order_name = data_order_names[nameIndex++];
+        // for each Data Size S2
+        for (int size : data_sizes){
+            // - Create an original array A1 with Data Order S1 and Data Size S2
+            int* original_array = new int[size];
+            GenerateData(original_array, size, (data_type)%4);
+            // for each Sorting Algorithm S3
+            for (string algorithm : algorithms){
+                // - Make a duplicate array A2 of the original array A1
+                int* duplicated_array = new int[size];
+                copy(original_array, original_array + size, duplicated_array);
+                // - Sort array A2 using the Sorting Algorithm S3, while:
+                    // + Measuring the running time (in millisecs), and
+                    // + Counting the number of comparisons in the algorithm
 
-        //Generate data
-        GenerateSortedData(sorted_array, n[i]);
-        GenerateNearlySortedData(nearly_sorted_array, n[i]);
-        GenerateReverseData(nearly_sorted_array, n[i]);
-        GenerateRandomData(randomized_array, n[i]);
+                    //Start measuring running time and counting comparisons
+                    auto start = chrono::high_resolution_clock::now();
+                    int comparisons = 0;
 
-        //Add array to holder and move on
-        sorted.push_back(sorted_array);
-        nearly_sorted.push_back(nearly_sorted_array);
-        reverse_sorted.push_back(reverse_sorted_array);
-        randomized.push_back(randomized_array);
+                    //call sort function
+                    SortData(duplicated_array, size, comparisons, (sort_type++)%algorithms.size());
+
+                    //Stop measuring time
+                    auto stop = chrono::high_resolution_clock::now();
+
+                    //Calculate running time
+                    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+                    
+                    fout << "[" << data_order_name << "] with size [" << size << "] using [" << algorithm << "]\t->Time: " << duration.count() << " miliseconds\t\tComparisons: " << comparisons << "\n";
+                    //
+                // - Take note of S1, S2, S3, running time and number of comparisons
+            }
+            // end for
+        }
+        // end for
+        data_type++;
     }
+    // end for
+    fout.close();
+}
 
-    //Check if data is successfully generated
-    //cout << randomized[5][499999];
+void SortData(int a[], int n, int &comparisons, int sortType)
+{
+    switch (sortType)
+    {
+    case 0:
+        SelectionSort(a, n, comparisons);
+        break;
+    case 1:
+        InsertionSort(a, n, comparisons);
+        break;
+    case 2:
+        ShellSort(a, n, comparisons);
+        break;
+    case 3:
+        BubbleSort(a, n, comparisons);
+        break;
+    case 4:
+        HeapSort(a, n, comparisons);
+        break;
+    case 5:
+        MergeSort(a, n, comparisons);
+        break;
+    case 6:
+        QuickSort(a, n, comparisons);
+        break;
+    case 7:
+        RadixSort(a, n, comparisons);
+        break;
+    case 8:
+        CountingSort(a, n, comparisons);
+        break;
+    case 9:
+        BinaryInsertionSort(a, n, comparisons);
+        break;
+    case 10:
+        ShakerSort(a, n, comparisons);
+        break;
+    case 11:
+        FlashSort(a, n, comparisons);
+        break;
+    default:
+        printf("Error: unknown sort type!\n");
+    }
+}
 
+void SelectionSort(int a[], int n, int &comparisons)
+{
+    for (int i = 0; i < n; i++){
+        int min_index = i;
+        for (int j = i + 1; j < n; j++){
+            if (a[j] < a[min_index]) min_index = j; comparisons++;
+        }
+        swap(a[i], a[min_index]);
+    }
+}
+
+void InsertionSort(int a[], int n, int &comparisons)
+{
+    for (int i = 1; i < n; i++){
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key){
+            a[j + 1] = a[j];
+            j--;
+            comparisons++;
+        }
+        comparisons++;
+        a[j + 1] = key;
+    }
+}
+
+void ShellSort(int a[], int n, int &comparions){
+    return;
+}
+
+void BubbleSort(int a[], int n, int &comparions){
+    return;
+}
+
+void HeapSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void MergeSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void QuickSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void RadixSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void CountingSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void BinaryInsertionSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void ShakerSort(int a[], int n, int &comparisons)
+{
+    return;
+}
+
+void FlashSort(int a[], int n, int &comparisons)
+{
+    return;
 }
---
+++ main.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include "data_generator.cpp"
+
+void SortData(int a[], int n, int &comparisons, int sortType);
+void SelectionSort(int a[], int n, int &comparions);
+void InsertionSort(int a[], int n, int &comparions);
+void ShellSort(int a[], int n, int &comparions);
+void BubbleSort(int a[], int n, int &comparions);
+void HeapSort(int a[], int n, int &comparions);
+void MergeSort(int a[], int n, int &comparions);
+void QuickSort(int a[], int n, int &comparions);
+void RadixSort(int a[], int n, int &comparions);
+void CountingSort(int a[], int n, int &comparions);
+void BinaryInsertionSort(int a[], int n, int &comparions);
+void ShakerSort(int a[], int n, int &comparions);
+void FlashSort(int a[], int n, int &comparions);
+
+
